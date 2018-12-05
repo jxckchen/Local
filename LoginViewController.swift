c61fb00e26b30f0d0e35d15011f41620f11f1186
jaxty <chenjackadam@gmail.com>
2018-12-04 20:45:50 -0500

    Initial commit

---
+++ LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Local
+//
+//  Created by Jack Chen on 12/4/18.
+//  Copyright © 2018 Jack Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+}
