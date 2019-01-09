8828d4dedf331d05cd8863516a4bdfd5c6b2ba83
jaxty <chenjackadam@gmail.com>
2019-01-09 12:55:25 -0500

    iqkeyboard works

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -10,9 +10,18 @@ import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setCustomBackgroundImage()
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.borderWidth = 1
     }
     
     
