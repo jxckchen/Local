e1c27f41ebeef77103491b7833d5ab9b77ebe85a
jaxty <chenjackadam@gmail.com>
2019-01-16 10:22:45 -0500

    centered tab bar items

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 
 class HomeViewController: UITabBarController{
     
+    @IBOutlet weak var barThing: UITabBarItem!
+    
     override func viewDidLoad() {
-        let tabBarItems = tabBar.items! as [UITabBarItem]
-        tabBarItems[0].imageInsets = UIEdgeInsets(top: 6,left: 0,bottom: -6,right: 0)
+        barThing.title = nil
+        barThing.imageInsets = UIEdgeInsets(top: 6,left: 0,bottom: -6,right: 0)
     }
     
     
