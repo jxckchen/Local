4ce6d1161ce5ae98585d0a14555f2e257cae485e
jaxty <chenjackadam@gmail.com>
2019-01-15 22:00:12 -0500

    attempting to center icon
    dont work yet

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -9,7 +9,12 @@
 import Foundation
 import UIKit
 
-class HomeViewController: UIViewController{
+class HomeViewController: UITabBarController{
+    
+    override func viewDidLoad() {
+        let tabBarItems = tabBar.items! as [UITabBarItem]
+        tabBarItems[0].imageInsets = UIEdgeInsets(top: 6,left: 0,bottom: -6,right: 0)
+    }
     
     
 }
