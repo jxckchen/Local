564e6f93e2add0e1db3e1ccbb4163a74a595f762
jaxty <chenjackadam@gmail.com>
2019-01-18 12:44:29 -0500

    Laid out segues

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -12,12 +12,15 @@ import UIKit
 class HomeViewController: UITabBarController{
     
     @IBOutlet weak var barThing: UITabBarItem!
+    @IBOutlet weak var profileImage: UIImageView!
+    let anyImage:UIImage = UIImage(named: "icons8-customer-30")!
     
     override func viewDidLoad() {
         barThing.title = nil
         barThing.imageInsets = UIEdgeInsets(top: 6,left: 0,bottom: -6,right: 0)
         setBackgroundGrey()
         self.tabBarController?.tabBar.barTintColor = UIColor.black
+//        profileImage.maskCircle(anyImage: anyImage.self)
     }
     
     
