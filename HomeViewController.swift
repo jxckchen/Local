1d19098374e739901165e2fc21c39594c4a3b2b0
jaxty <chenjackadam@gmail.com>
2019-02-05 08:46:21 -0500

    added logout function

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -22,6 +22,30 @@ class HomeViewController: UIViewController{
         barThing.imageInsets = UIEdgeInsets(top: 6,left:0,bottom: -6,right: 0)
         setBackgroundGrey()
         self.tabBarController?.tabBar.barTintColor = UIColor.black
+    }
+    
+    
+    @IBAction func logout(_ sender: Any) {
+        let alertController = UIAlertController(title: "Logout", message: "Are you sure you want to logout?", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        self.present(alertController, animated: true, completion: nil)
+        let logoutAction = UIAlertAction(title: "Sign out", style: .default, handler: {(_: UIAlertAction!) in
+            self.logoutFunction()
+        })
+        alertController.addAction(defaultAction)
+        alertController.addAction(logoutAction)
+    }
+    
+    func logoutFunction() {
+        do {
+            try Auth.auth().signOut()
         }
+        catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let initial = storyboard.instantiateInitialViewController()
+        UIApplication.shared.keyWindow?.rootViewController = initial
+    }
     
 }
