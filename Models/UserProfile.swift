f6b531e3b18f7bc31113503772fb7c594f53d8cd
jaxty <chenjackadam@gmail.com>
2019-03-08 08:59:35 -0500

    fixed URL and URLsession issues

--- Models/UserProfile.swift
+++ Models/UserProfile.swift
@@ -11,9 +11,9 @@ import Foundation
 class UserProfile {
     var uid:String
     var username:String
-    var photoURL:URLSession
+    var photoURL:URL
     
-    init(uid:String, username:String,photoURL:URLSession) {
+    init(uid:String, username:String,photoURL:URL) {
         self.uid = uid
         self.username = username
         self.photoURL = photoURL
